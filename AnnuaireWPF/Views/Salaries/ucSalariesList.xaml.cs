072ec15a63d9c0b1d5a628adc67c3cac7d87b426
guimey <guillaume.meyer@viacesi.fr>
2024-07-15 16:40:56 +0200

    barre de recherche + auth (wip)

--- AnnuaireWPF/Views/Salaries/ucSalariesList.xaml.cs
+++ AnnuaireWPF/Views/Salaries/ucSalariesList.xaml.cs
@@ -2,6 +2,7 @@
 using AnnuaireWPF.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +36,36 @@ namespace AnnuaireWPF.Views.Salaries
         private void SalarieCreate_Click(object sender, RoutedEventArgs e)
         {
             MainViewModel.Instance.CreateSalarie();
+        }
+
+        public void SearchList(string search, ObservableCollection<Salarie> list)
+        {
+            SalariesViewModel.Instance.ListeSalaries.Clear();
+            var siteId = SalariesViewModel.Instance.SiteId;
+            var serviceId = SalariesViewModel.Instance.ServiceId;
+
+           
+            list = new ObservableCollection<Salarie>(list.Where(s => s.Nom.ToLower().Contains(search.ToLower()) ||
+                                                            s.Email.ToLower().Contains(search.ToLower()) ||
+                                                            s.TelFix.ToLower().Contains(search.ToLower()) ||
+                                                            s.TelPort.ToLower().Contains(search.ToLower()) ||
+                                                            s.Prenom.ToLower().Contains(search.ToLower()) ||
+                                                            s.Service.NomService.ToLower().Contains(search.ToLower()) ||
+                                                            s.Site.Ville.ToLower().Contains(search.ToLower())));
+
+
+            foreach (var s in list)
+            {
+                SalariesViewModel.Instance.ListeSalaries.Add(s);
+            }
+
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var txt = ((TextBox)sender).Text;
+            var bsl = SalariesViewModel.Instance.BaseSalarieList;
+
+            SearchList(txt, bsl);
         }
     }
 }
